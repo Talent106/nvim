a2d9276fe9892fd81a67772e6962a9f039c70f7f
Talent106 <kimurayu106@outlook.com>
2024-01-05 18:28:34 -0300

    fix: remove blade parser

--- lua/default/treesitter-setup.lua
+++ lua/default/treesitter-setup.lua
@@ -1,19 +1,19 @@
 -- ADD BLADE SUPPORT
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.blade = {
-  install_info = {
-    url = "https://github.com/EmranMR/tree-sitter-blade",
-    files = {"src/parser.c"},
-    branch = "main",
-  },
-  filetype = "blade"
-}
-
-vim.filetype.add({
-  pattern = {
-    ['.*%.blade%.php'] = 'blade',
-  }
-})
+-- local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+-- parser_config.blade = {
+--   install_info = {
+--     url = "https://github.com/EmranMR/tree-sitter-blade",
+--     files = {"src/parser.c"},
+--     branch = "main",
+--   },
+--   filetype = "blade"
+-- }
+--
+-- vim.filetype.add({
+--   pattern = {
+--     ['.*%.blade%.php'] = 'blade',
+--   }
+-- })
 -----
 
 require('nvim-treesitter.configs').setup {
@@ -32,7 +32,7 @@ require('nvim-treesitter.configs').setup {
     'vim',
     'dot',
     'bash',
-    'blade',
+    -- 'blade',
     'angular',
   },
   auto_install = true,
