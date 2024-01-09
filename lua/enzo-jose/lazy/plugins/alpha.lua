7c92be9f30983f74b076986270ffd0fbea989939
Talent106 <kimurayu106@outlook.com>
2024-01-09 00:06:46 -0300

    fix: remove useless fortune call by alpha

--- lua/enzo-jose/lazy/plugins/alpha.lua
+++ lua/enzo-jose/lazy/plugins/alpha.lua
@@ -3,6 +3,7 @@ return {
   config = function()
     local alpha = require 'alpha'
     local dashboard = require 'alpha.themes.dashboard'
+
     dashboard.section.header.val = {
       [[                               __                ]],
       [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
@@ -11,6 +12,7 @@ return {
       [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
       [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
     }
+
     dashboard.section.buttons.val = {
       dashboard.button("CTRL-P", "  Open Git Files Search"),
       dashboard.button("SPC-pf", "  Open find Files Search"),
@@ -18,12 +20,6 @@ return {
       dashboard.button("SPC-e", "  Open file tree"),
       dashboard.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
     }
-    local handle = io.popen('fortune')
-    local fortune = handle:read("*a")
-    handle:close()
-    dashboard.section.footer.val = fortune
-
-    dashboard.config.opts.noautocmd = true
 
     alpha.setup(dashboard.config)
   end
