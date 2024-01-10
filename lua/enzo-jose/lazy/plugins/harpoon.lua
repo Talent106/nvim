67f79ab87c51108450f2e79c504c956abdeab28d
Talent106 <kimurayu106@outlook.com>
2024-01-10 20:59:42 -0300

    feat (harpoon): remove some keybinds and add descriptions

--- lua/enzo-jose/lazy/plugins/harpoon.lua
+++ lua/enzo-jose/lazy/plugins/harpoon.lua
@@ -6,19 +6,17 @@ return {
 
     harpoon:setup()
 
-    vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-    vim.keymap.set("n", "<leader>A", function() harpoon:list():remove() end)
-    vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-    vim.keymap.set("n", "<leader>al", function() harpoon:list():clear() end)
+    vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end, { desc = 'Add to Harpoon list' })
+    vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = 'Open Harpoon Window' })
 
-    vim.keymap.set("n", "<leader>h", function() harpoon:list():select(1) end)
-    vim.keymap.set("n", "<leader>j", function() harpoon:list():select(2) end)
-    vim.keymap.set("n", "<leader>k", function() harpoon:list():select(3) end)
-    vim.keymap.set("n", "<leader>l", function() harpoon:list():select(4) end)
+    vim.keymap.set("n", "<leader>h", function() harpoon:list():select(1) end, { desc = 'Go to Harpoon mark 1' })
+    vim.keymap.set("n", "<leader>j", function() harpoon:list():select(2) end, { desc = 'Go to Harpoon mark 2' })
+    vim.keymap.set("n", "<leader>k", function() harpoon:list():select(3) end, { desc = 'Go to Harpoon mark 3' })
+    vim.keymap.set("n", "<leader>l", function() harpoon:list():select(4) end, { desc = 'Go to Harpoon mark 4' })
 
-    vim.keymap.set("n", "<leader>H", function() harpoon:list():select(5) end)
-    vim.keymap.set("n", "<leader>J", function() harpoon:list():select(6) end)
-    vim.keymap.set("n", "<leader>K", function() harpoon:list():select(7) end)
-    vim.keymap.set("n", "<leader>L", function() harpoon:list():select(8) end)
+    vim.keymap.set("n", "<leader>H", function() harpoon:list():select(5) end, { desc = 'Go to Harpoon mark 5' })
+    vim.keymap.set("n", "<leader>J", function() harpoon:list():select(6) end, { desc = 'Go to Harpoon mark 6' })
+    vim.keymap.set("n", "<leader>K", function() harpoon:list():select(7) end, { desc = 'Go to Harpoon mark 7' })
+    vim.keymap.set("n", "<leader>L", function() harpoon:list():select(8) end, { desc = 'Go to Harpoon mark 8' })
   end,
 }
