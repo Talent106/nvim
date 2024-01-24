fdd08d108c7a85bca74ed7274c1cedb567cc2e71
Talent106 <kimurayu106@outlook.com>
2024-01-24 19:59:32 -0300

    feat: add quick notes keymaps and settings

--- lua/enzo-jose/lazy/plugins/local-plugins.lua
+++ lua/enzo-jose/lazy/plugins/local-plugins.lua
@@ -2,7 +2,14 @@ return {
   {
     dir = '~/Development/neovim-plugins/quick-notes.nvim',
     config = function ()
-      -- require('quick-notes'):setup()
+      local quickNotes = require('quick-notes');
+
+      quickNotes:setup({open_command = 'vsplit', notes_dir = '/Documentos/Notes'});
+
+      vim.keymap.set("n", "<leader>Qq", function() quickNotes:toggle() end, { desc = 'Toggle Quick Menu' });
+      vim.keymap.set("n", "<leader>Ql", function() quickNotes:openLastNote() end, { desc = 'Open last open note' });
+      vim.keymap.set("n", "<leader>Qn", function() quickNotes:new() end, { desc = 'Create new note on notes_dir' });
+      vim.keymap.set("n", "<leader>Qb", function() quickNotes:browse() end, { desc = 'Browse to notes dir using Netrw' });
     end
   },
 }
