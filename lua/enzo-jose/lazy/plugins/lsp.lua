5f1da92537c91982397650819d2c1f7fe071d639
Talent106 <kimurayu106@outlook.com>
2024-07-10 18:01:11 -0300

    feat: change default format config

--- lua/enzo-jose/lazy/plugins/lsp.lua
+++ lua/enzo-jose/lazy/plugins/lsp.lua
@@ -41,7 +41,7 @@ return {
                 '[W]orkspace [L]ist Folders')
 
             vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-                vim.lsp.buf.format({ formatting_options = { tabSize = 4 } })
+                vim.lsp.buf.format({ formatting_options = { tabSize = 4, insertSpaces = true } })
             end, { desc = 'Format current buffer with LSP' })
         end
 
